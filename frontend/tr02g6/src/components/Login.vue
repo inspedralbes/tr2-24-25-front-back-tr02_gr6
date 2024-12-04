6ed97f6c9ed51a6754e86f5b6297f544d2ee90b4
a23kaeguapio <a23kaeguapio@inspedralbes.cat>
2024-12-04 23:41:15 +0100

    estructura base del login elaborada

---
+++ frontend/tr02g6/src/components/Login.vue
@@ -0,0 +1,93 @@
+<template>
+    <div  justify="center" class="align-center justify-center">
+        <slot name="logo">
+      <v-img
+        class="mx-auto"
+        max-width="228"
+        src="https://icones.pro/wp-content/uploads/2021/02/icone-utilisateur-bleu.png"
+      ></v-img>
+        </slot>
+  
+        <v-form @submit.prevent="submitLogin()" ref="form">
+      <v-card
+        class="mx-auto pa-12 pb-8"
+        elevation="8"
+        max-width="448"
+        rounded="lg"
+      >
+        <div class="text-subtitle-1 text-medium-emphasis">Email</div>
+  
+        <v-text-field
+                v-model="identifier"
+          density="compact"
+          :placeholder="identifierPlaceholder"
+          :rules="[v => !!v || 'Item is required']"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+        ></v-text-field>
+  
+        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+          Contraseña
+  
+         
+        </div>
+  
+        <v-text-field
+                v-model="password"
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          :placeholder="passwordPlaceholder"
+          :rules="[v => !!v || 'Item is required']"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
+  
+        <v-btn
+          block
+          class="mb-8"
+          color="blue"
+          size="large"
+          variant="tonal"
+          type="submit"
+        >
+          Log In
+        </v-btn>
+      </v-card>
+        </v-form>
+    </div>
+  </template>
+  <script setup>
+      import { ref } from 'vue'
+  
+      defineProps({
+          identifierPlaceholder: {
+              type: String,
+              default: 'email@example.com',
+          },
+          passwordPlaceholder: {
+              type: String,
+              default: 'Enter your password',
+          }
+      })
+  
+      const emits = defineEmits(['submit-login'])
+  
+       const visible =  ref(false)
+       const form = ref(null)
+  
+       const identifier = ref('')
+       const password = ref('')
+  
+       const submitLogin = async () => {
+  
+          const { valid } = await form.value.validate()
+  
+          if (valid) {
+              emits('submit-login', { identifier: identifier.value, password: password.value })
+          }
+       }
+  </script>
+  <style scoped>
+  </style>
