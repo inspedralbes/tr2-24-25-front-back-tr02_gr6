22a41248fc1a454525de90f2d62a835839752c1f
a23matcasmar <a23matcasmar@inspedralbes.cat>
2024-12-10 11:41:27 +0100

    hice los fecth para los formularios

--- frontend/tr02g6/src/components/Formulario_personalidad.vue
+++ frontend/tr02g6/src/components/Formulario_personalidad.vue
@@ -2,12 +2,12 @@
 <script setup>
 import { ref, watch } from 'vue'
 const requiredRule = [
-  usuario => !!usuario || 'Este campo es obligatorio',
+  correo => !!correo || 'Este campo es obligatorio',
 
 ];
-const correos = ref(['correo1', 'correo2', 'correo3', 'correo4', 'correo'])
-const nombre = ref(['Luis', 'Pedro', 'Pablo', 'Pablow', 'Pablo3'])
-const usuario = ref();
+const correos = ref([])
+const alumnos = ref([])
+const correo = ref();
 const cae_bien = ref([]);
 const cae_no_bien = ref([]);
 const difunde_rumores = ref([]);
@@ -27,6 +27,7 @@ const limitSelections = (selectedItems) => {
     selectedItems.shift();
   }
 };
+
 const listaarrays = [
   cae_bien,
   cae_no_bien,
@@ -47,12 +48,12 @@ listaarrays.forEach((array) => {
   });
 });
 
-function validarform() {
+/*function validarform() {
   const valido = listaarrays.every(listaarrays => listaarrays.value.length === 3);
   if (!valido) {
     alert("Todos los campos deben tener exactamente 3 selecciones.");
   }
-  else if (usuario.value == null) {
+  else if (correo.value == null) {
     alert('Debe seleccionar un correo.');
   }
   else if (!terminosycondiciones.value) {
@@ -60,7 +61,7 @@ function validarform() {
   }
  else{
   const formData = {
-    usuario: usuario.value,
+    correo: correo.value,
     cae_bien: cae_bien.value,
     cae_no_bien: cae_no_bien.value,
     difunde_rumores: difunde_rumores.value,
@@ -77,25 +78,92 @@ function validarform() {
   console.log("Formulario enviado con éxito:", formData);
   dialog.value = true;
 }
-};
+};*/
 
+async function fecthGetCorreos() {
+  try {
+    correos.value = await getcorreos();
+  }
+  catch (error) {
+    console.error('Error al obtener correos', error);
+  }
+}
+
+async function fecthGetAlumnos() {
+  try {
+    alumnos.value = await getAlumnos();
+  }
+  catch (error) {
+    console.error('Error al obtener correos', error);
+  }
+}
+
+async function fetchPostResultats() {
+  try {
+    const valido = listaarrays.every(listaarrays => listaarrays.value.length === 3);
+    if (!valido) {
+      alert("Todos los campos deben tener exactamente 3 selecciones.");
+    }
+    else if (correo.value == null) {
+      alert('Debe seleccionar un correo.');
+    }
+    else if (!terminosycondiciones.value) {
+      alert('Debe aceptar los terminos y condiciones.');
+    }
+    else {
+      const resultats = await postResultats(
+        correo.value,
+        cae_bien.value,
+        cae_no_bien.value,
+        difunde_rumores.value,
+        ayuda_demás.value,
+        da_empujones.value,
+        no_deja_participar.value,
+        anima_demas.value,
+        insulta.value,
+        victima_empujones.value,
+        victima_insultar.value,
+        victima_no_deja_participar.value,
+        amigos.value,
+      );
+      console.log("Formulario enviado con éxito:", resultats);
+      correo.value = '';
+      cae_bien.value = '';
+      cae_no_bien.value = '';
+      difunde_rumores.value = '';
+      ayuda_demás.value = '';
+      da_empujones.value = '';
+      no_deja_participar.value = '';
+      anima_demas.value = '';
+      insulta.value = '';
+      victima_empujones.value = '';
+      victima_insultar.value = '';
+      victima_no_deja_participar.value = '';
+      amigos.value = '';
+      dialog.value = true;
+    }
+  }
+  catch (error) {
+    console.error('Hubo problemas al enviar los resultados.', error);
+  }
+  }
 </script>
 
 <template id="cuerpo">
   <div id="formulario">
     <v-card-title>CESC- Conducta y Experiencias Sociales en Clase (ESO)</v-card-title>
     <v-select
-      v-model="usuario"
+      v-model="correo"
       label="Correo"
       :rules="requiredRule"
-      :items="correos"
+      :items="fecthGetCorreos"
     />
     <v-card-text>Es obligatorio seleccionar tres opciones en cada pregunta</v-card-text>
     <div id="preguntasTest">
       <v-select
         v-model="cae_bien"
         label="¿Quién me cae bien?"
-        :items="nombre"
+        :items="fecthGetAlumnos"
         :rules="requiredRule"
         :multiple="true"
       />
@@ -114,7 +182,7 @@ function validarform() {
 
       <v-select
         v-model="cae_no_bien"
-        :items="nombre"
+        :items="fecthGetAlumnos"
         label="¿Quién NO me cae bien?"
         :rules="requiredRule"
         :multiple="true"
@@ -134,7 +202,7 @@ function validarform() {
 
       <v-select
         v-model="difunde_rumores"
-        :items="nombre"
+        :items="fecthGetAlumnos"
         label="¿Quién difunde rumores?"
         :rules="requiredRule"
         :multiple="true"
@@ -154,7 +222,7 @@ function validarform() {
 
       <v-select
         v-model="ayuda_demás"
-        :items="nombre"
+        :items="fecthGetAlumnos"
         label="¿Quien ayuda a los demás?"
         :rules="requiredRule"
         :multiple="true"
@@ -174,7 +242,7 @@ function validarform() {
 
       <v-select
         v-model="da_empujones"
-        :items="nombre"
+        :items="fecthGetAlumnos"
         label="¿Quien da empujones?"
         :rules="requiredRule"
         :multiple="true"
@@ -194,7 +262,7 @@ function validarform() {
 
       <v-select
         v-model="no_deja_participar"
-        :items="nombre"
+        :items="fecthGetAlumnos"
         label="¿Quien no deja participar?"
         :rules="requiredRule"
         :multiple="true"
@@ -214,7 +282,7 @@ function validarform() {
 
       <v-select
         v-model="anima_demas"
-        :items="nombre"
+        :items="fecthGetAlumnos"
         label="¿Quien anima a los demás?"
         :rules="requiredRule"
         :multiple="true"
@@ -234,7 +302,7 @@ function validarform() {
 
       <v-select
         v-model="insulta"
-        :items="nombre"
+        :items="fecthGetAlumnos"
         label="¿Quien insulta?"
         :rules="requiredRule"
         :multiple="true"
@@ -254,7 +322,7 @@ function validarform() {
 
       <v-select
         v-model="victima_empujones"
-        :items="nombre"
+        :items="fecthGetAlumnos"
         label="¿A quién dan empujones?"
         :rules="requiredRule"
         :multiple="true"
@@ -274,7 +342,7 @@ function validarform() {
 
       <v-select
         v-model="victima_insultar"
-        :items="nombre"
+        :items="fecthGetAlumnos"
         label="¿A quién insultan o ridiculizan?"
         :rules="requiredRule"
         :multiple="true"
@@ -294,7 +362,7 @@ function validarform() {
 
       <v-select
         v-model="victima_no_deja_participar"
-        :items="nombre"
+        :items="fecthGetAlumnos"
         label="¿A quién no dejan participar?"
         :rules="requiredRule"
         :multiple="true"
@@ -314,7 +382,7 @@ function validarform() {
 
       <v-select
         v-model="amigos"
-        :items="nombre"
+        :items="fecthGetAlumnos"
         label="¿Quienes son mis amigos/amigas?"
         :rules="requiredRule"
         :multiple="true"
@@ -338,7 +406,7 @@ function validarform() {
     />
     <v-btn
       type="submit"
-      @click="validarform"
+      @click="fetchPostResultats"
     >
       Enviar
     </v-btn>
@@ -352,11 +420,11 @@ function validarform() {
         text="Los datos introducidos son completamente privados."
         title="¡Gracias por participar!"
       >
-        <template v-slot:actions>
+        <template #actions>
           <v-btn
             text="Vale"
             @click="dialog = false"
-          ></v-btn>
+          />
         </template>
       </v-card>
     </v-dialog>
@@ -367,18 +435,20 @@ function validarform() {
 #formulario {
   background: linear-gradient(rgb(255, 254, 205), rgb(187, 252, 255), rgb(246, 219, 255));
 }
+
 #preguntasTest {
   display: grid;
   grid-template-columns: 1.5fr 1fr 1fr 1fr;
 }
 
-#preguntasTest >* {
+#preguntasTest>* {
   margin: 5px;
 }
 
-#formulario > * {
+#formulario>* {
   margin: 5px
 }
+
 #mensajeagradecimiento {
   background-image: url("https://i.pinimg.com/736x/89/94/46/8994461746191a7480ceea961a045852.jpg");
 }
