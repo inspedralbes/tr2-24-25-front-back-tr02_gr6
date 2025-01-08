5dec8b24b9026b59fc66aa69a18cdc4ac6ae03a4
a23kaeguapio <a23kaeguapio@inspedralbes.cat>
2025-01-08 12:08:31 +0100

    cambiando colores del login

--- frontend/tr02g6/src/components/LoginIRegister.vue
+++ frontend/tr02g6/src/components/LoginIRegister.vue
@@ -1,106 +1,157 @@
 <template>
-  <v-container class="d-flex align-center justify-center">
-    <v-row class="ma-0" align="center">
-      <v-col cols="12" md="6" class="mx-auto ma-0">
-        <v-card class="elevation-6 mx-auto ma-0" max-width="600px">
-          <v-window justify="center" v-model="step">
+  <v-container class="d-flex align-center justify-center ">
+    <v-row class="ma-0" justify="center">
+      <v-col cols="12" md="8" lg="6">
+        <v-card color="#FFF8E1" class="elevation-6 mx-auto" max-width="600">
+          <v-window v-model="step" class="pa-3">
             <!-- Login -->
             <v-window-item :value="1">
-              <v-row align="center" justify="center">
-                <v-col cols="12" md="12">
-                  <v-card-text class="mt-1 mb-4">
+              <v-row justify="center">
+                <v-col cols="12" sm="10">
+                  <v-card-text>
                     <slot name="logo">
-                      <v-img class="mx-auto mb-2" max-width="150" src="/login.png"></v-img>
+                      <v-img
+                        class="rounded-circle mx-auto mb-4"
+                        max-width="150"
+                        src="/login1.png"
+                      ></v-img>
                     </slot>
                     <v-form @submit.prevent="handleLogin" ref="form">
-                      <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="420" rounded="lg">
-                        <div class="text-subtitle-1 text-medium-emphasis">
-                          Email
-                        </div>
-                        <v-text-field v-model="professor.email" density="compact" :placeholder="identifierPlaceholder"
-                          :rules="[(v) => !!v || 'Aquest camp és obligatori']" prepend-inner-icon="mdi-email-outline"
-                          variant="outlined"></v-text-field>
+                      <div class="text-subtitle-1 text-medium-emphasis mb-2">
+                        Email
+                      </div>
+                      <v-text-field
+                        v-model="professor.email"
+                        density="compact"
+                        placeholder="Introduce tu email"
+                        :rules="[(v) => !!v || 'Aquest camp és obligatori']"
+                        prepend-inner-icon="mdi-email-outline"
+                        variant="outlined"
+                      ></v-text-field>
 
-                        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                          Contrasenya
-                        </div>
+                      <div class="text-subtitle-1 text-medium-emphasis mt-4 mb-2">
+                        Contrasenya
+                      </div>
+                      <v-text-field
+                        v-model="professor.contrassenya"
+                        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                        :type="visible ? 'text' : 'password'"
+                        density="compact"
+                        placeholder="Introduce tu contraseña"
+                        :rules="[(v) => !!v || 'Aquest camp és obligatori']"
+                        prepend-inner-icon="mdi-lock-outline"
+                        variant="outlined"
+                        @click:append-inner="visible = !visible"
+                      ></v-text-field>
 
-                        <v-text-field v-model="professor.contrassenya" :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'
-                          " :type="visible ? 'text' : 'password'" density="compact" :placeholder="passwordPlaceholder"
-                          :rules="[(v) => !!v || 'Aquest camp és obligatori']" prepend-inner-icon="mdi-lock-outline"
-                          variant="outlined" @click:append-inner="visible = !visible"></v-text-field>
+                      <v-row justify="center" class="mt-4">
+                        <v-btn
+                          color="#FF8F00"
+                          size="large"
+                          variant="tonal"
+                          type="submit"
+                          class="mr-4"
+                        >
+                          Log In
+                        </v-btn>
+                        <v-btn size="large" tile outlined @click="step++">
+                          Sign Up
+                        </v-btn>
+                      </v-row>
 
-                        <v-row justify="center" align="center" class="mt-4">
-                          <v-btn color="blue" size="large" variant="tonal" type="submit" class="mr-4">
-                            Log In
-                          </v-btn>
-                          <v-btn tile outlined @click="step++"> Sign Up </v-btn>
-                        </v-row>
-                        <v-alert v-if="errorMessage" type="error" class="mt-3">
-                          {{ errorMessage }}
-                        </v-alert>
-                      </v-card>
+                      <v-alert
+                        v-if="errorMessage"
+                        type="error"
+                        class="mt-3"
+                      >
+                        {{ errorMessage }}
+                      </v-alert>
                     </v-form>
                   </v-card-text>
                 </v-col>
               </v-row>
             </v-window-item>
-            <!-- Registre -->
+
+            <!-- Registro -->
             <v-window-item :value="2">
-              <v-row align="center" justify="center">
-                <v-col cols="12" md="6" class="mx-auto ma-0">
-                  <v-card-text class="mt-3 mb-4">
-                    <v-btn 
-                    class="ma-2"
-                    color="blue-lighten-2"
-                    @click="step--">
+              <v-row justify="center">
+                <v-col cols="12" sm="10">
+                  <v-card-text>
+                    <v-btn
+                      class="ma-2"
+                      color="amber-darken-3"
+                      @click="step--"
+                    >
                       <v-icon icon="mdi-arrow-left" start></v-icon>
                     </v-btn>
-                    <h3 class="text-center">Crea un compte</h3>
-                    <v-spacer class="ma-7"></v-spacer>
-                    <v-row align="center" justify="center">
-                      <v-col cols="12" sm="8">
-                        <v-row>
-                          <v-col cols="12" sm="6">
-                            <div class="text-subtitle-1 text-medium-emphasis">
-                              Nom
-                            </div>
-                            <v-text-field v-model="professor.nom" density="compact" :placeholder="'Nom'" :rules="[
-                              (v) => !!v || 'Aquest camp és obligatori',
-                            ]" prepend-inner-icon="mdi-account-outline" variant="outlined" class="mt-2" />
-                          </v-col>
-                          <v-col cols="12" sm="6">
-                            <div class="text-subtitle-1 text-medium-emphasis">
-                              Cognoms
-                            </div>
-                            <v-text-field v-model="professor.cognoms" density="compact" :placeholder="'Cognoms'" :rules="[
-                              (v) => !!v || 'Aquest camp és obligatori',
-                            ]" prepend-inner-icon="mdi-account-outline" variant="outlined" class="mt-2" />
-                          </v-col>
-                        </v-row>
-
-                        <div class="text-subtitle-1 text-medium-emphasis">
-                          Email
+                    <h2 class="text-center mb-6">Crea un compte</h2>
+                    <v-row>
+                      <v-col cols="12" sm="6">
+                        <div class="text-subtitle-1 text-medium-emphasis mb-2">
+                          Nom
                         </div>
-                        <v-text-field v-model="professor.email" density="compact" :placeholder="'email@example.com'"
-                          :rules="[(v) => !!v || 'Aquest camp és obligatori']" prepend-inner-icon="mdi-email-outline"
-                          variant="outlined" class="mt-2" />
-
-                        <div class="text-subtitle-1 text-medium-emphasis">
-                          Contrasenya
+                        <v-text-field
+                          v-model="professor.nom"
+                          density="compact"
+                          placeholder="Nom"
+                          :rules="[(v) => !!v || 'Aquest camp és obligatori']"
+                          prepend-inner-icon="mdi-account-outline"
+                          variant="outlined"
+                        />
+                      </v-col>
+                      <v-col cols="12" sm="6">
+                        <div class="text-subtitle-1 text-medium-emphasis mb-2">
+                          Cognoms
                         </div>
-                        <v-text-field v-model="professor.contrassenya" :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'
-                          " :type="visible ? 'text' : 'password'" density="compact"
-                          :placeholder="'Insereix contrasenya'" :rules="[(v) => !!v || 'Aquest camp és obligatori']"
-                          prepend-inner-icon="mdi-lock-outline" variant="outlined" class="mt-2 custom-placeholder"
-                          @click:append-inner="visible = !visible" />
-
-                        <v-btn size="large" variant="tonal" type="submit" class="mr-4 mt-4" color="blue" block
-                          @click="handleRegister">
-                          Sign up
-                        </v-btn>
+                        <v-text-field
+                          v-model="professor.cognoms"
+                          density="compact"
+                          placeholder="Cognoms"
+                          :rules="[(v) => !!v || 'Aquest camp és obligatori']"
+                          prepend-inner-icon="mdi-account-outline"
+                          variant="outlined"
+                        />
                       </v-col>
                     </v-row>
+
+                    <div class="text-subtitle-1 text-medium-emphasis mt-4 mb-2">
+                      Email
+                    </div>
+                    <v-text-field
+                      v-model="professor.email"
+                      density="compact"
+                      placeholder="email@example.com"
+                      :rules="[(v) => !!v || 'Aquest camp és obligatori']"
+                      prepend-inner-icon="mdi-email-outline"
+                      variant="outlined"
+                    />
+
+                    <div class="text-subtitle-1 text-medium-emphasis mt-4 mb-2">
+                      Contrasenya
+                    </div>
+                    <v-text-field
+                      v-model="professor.contrassenya"
+                      :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                      :type="visible ? 'text' : 'password'"
+                      density="compact"
+                      placeholder="Insereix contrasenya"
+                      :rules="[(v) => !!v || 'Aquest camp és obligatori']"
+                      prepend-inner-icon="mdi-lock-outline"
+                      variant="outlined"
+                      @click:append-inner="visible = !visible"
+                    />
+
+                    <v-btn
+                      color="#FF8F00"
+                      size="large"
+                      variant="tonal"
+                      type="submit"
+                      class="mt-4"
+                      block
+                      @click="handleRegister"
+                    >
+                      Sign up
+                    </v-btn>
                   </v-card-text>
                 </v-col>
               </v-row>
